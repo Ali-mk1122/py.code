d04548c85e01227643632e6b1048901cd7b893e0
Ali-mk1122 <malekalireza774@gmail.com>
2024-09-24 17:32:56 +0330

    Add files via upload
    This code is for age estimation

---
+++ age.py
@@ -0,0 +1,11 @@
+a = int(input("enter your birth year : "))
+b = int(input("enter your birth month : "))
+c = int(input("enter your birth day : "))
+A = int(input("enter date of the present year : "))
+B = int(input("enter date of the present month : "))
+C = int(input("enter date of the day : "))
+year = A - a
+month = B - b
+day = C - c
+print("year =",year,"/","month =",month,"/","day =",day)
+
