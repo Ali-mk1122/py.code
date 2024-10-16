1e9ecfc3b5ac8494982af8f1b630154941fb834b
Ali-mk1122 <malekalireza774@gmail.com>
2024-10-16 12:00:54 +0330

    Add files via upload

--- inheritance/school.py
+++ inheritance/school.py
@@ -2,26 +2,31 @@ class Person:
     def __init__(self,name,age):
         self.name = name
         self.age = age
-    def display_info():
+    def display_info(self):
         print(self.name,self.age)
 
-    class Student(Person):
-        def __init__(self,geade,student_id):
-            super().__init__(self.name,self.age)
-            self.student = student
-            self.student_id = student_id
+class Student(Person):
+    def __init__(self,name,age,grade,student_id):
+        super().__init__(name,age)
+        self.grade = grade
+        self.student_id = student_id
         
-        def display_info(self,student,student_id):
-            super().display_info(self.name,self.age)
-            print(self.name,self.age,self.student,self.student_id)
+    def display_info(self):
+        print(self.grade,self.student_id)
 
 
-    class Teacher(Person):
-        def __init__(self,subject,salary):
-            super().__init__(self.name,self.age)
-            self.subject = subject
-            self.salary = salary
+class Teacher(Person):
+    def __init__(self,name,age,subject,salary):
+        super().__init__(name,age)
+        self.subject = subject
+        self.salary = salary
         
-        def display_info(self,subject,salary):
-            super().display_info(self.name,self.age)  
-            print(self.name,self.age,self.subject,self.salary)
+    def display_info(self):  
+            print(self.subject,self.salary)
+
+
+
+# n = Student("ali",17,"diplima",333)
+# m = Teacher("abbas",33,"english_teacher",20000000)
+# n.display_info()
+# m.display_info()
